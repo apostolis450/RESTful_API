403cab964d8fb62933a45b5274245cb36c67f5b0
Apostolis <apostolis450@gmail.com>
2021-02-07 23:52:28 +0200

    Added more functionalities and sql db

--- test.py
+++ test.py
@@ -2,8 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "frutotrela", "views": 1000})
-print(response.json())
+"""
+data = [ {"name": "frutotrela", "views": 10, "likes": 1000,},
+		 {"name": "mpampinos", "views": 100, "likes": 1000,},
+         {"name": "nikolakis", "views": 1000, "likes": 1000,} ]
+
+for i in range(len(data)):
+	response = requests.put(BASE + "video/" + str(i), data[i])
+	print(response.json())	
+
+
 input()
-response = requests.get(BASE + "video/1")
+response = requests.get(BASE + "video/2")
+print(response)
+"""
+response = requests.patch(BASE + "video/1", {"views":10})
 print(response.json())
